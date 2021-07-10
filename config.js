68205dbe1ffa1be4d1abfb7b07e5519051462339
1231kjsad <87238909+1231kjsad@users.noreply.github.com>
2021-07-10 23:57:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Nhím à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introDesc: `Công chúa nhỏ đang giận anh phải khum`,
+    btnIntro: 'đúng roài',
+    title: 'Người yêu đừng giận anh nữa nhé, a thương bé nhiều lắm lắm ',
+    desc: 'giờ a sẽ cố gắng làm cho em cười nhiều hơn nhaaa ',
+    btnYes: 'Okeee bae, hết giận roài',
+    btnNo: 'Không, mặc kệ mày',
+    question:'giờ công chúa xem phim nha',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://facebook.com' //https://www.facebook.com/
 }
