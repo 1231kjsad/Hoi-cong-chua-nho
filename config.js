1db56ec103cb3c4ab01802655c6ccf049bc3269a
1231kjsad <87238909+1231kjsad@users.noreply.github.com>
2021-07-10 23:53:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,1 @@
-const CONFIG = {
-    introTitle: 'Nhím à!',
-    introDesc: `Công chúa nhỏ ơi
-    em đang giận anh phải khum`,
-    btnIntro: 'Đúng roài',
-    title: 'Babi à đừng buồn anh nữa nhé, Thương babii nhiều lắm',
-    desc: 'Từ giờ a sẽ cố gắng nhiều nữa để em được cười nhiều nhé',
-    btnYes: 'Heheheeee Nhím đã vui trở lại',
-    btnNo: 'Không, ghét roài',
-    question:'giờ em hãy cười thật nhiều và hạnh phúc nhiều nhaaa, giờ xem phim thôi',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'giờ midnh xem phim nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3 <3',
-    messLink: 'http://facebook.com' https://www.facebook.com/
-}
+
